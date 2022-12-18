92b52fde20177628a5877609f9b3d0504a81e865
falkanx <116848375+falkanx@users.noreply.github.com>
2022-12-18 19:38:20 +0300

    Add files via upload

---
+++ Arrays/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // Unidimensional Array
+            // Beginner
+            string[] students = new string[3];
+            students[0] = "Furkan";
+            students[1] = "Cagri";
+            students[2] = "Kemal";
+            // Elementary
+            string[] students1 = new string[3]{ "Furkan", "Cagri", "Kemal" };
+            // Intermediate
+            string[] students2 = new[] { "Furkan", "Cagri", "Kemal" };
+            // Advanced
+            string[] students3 = { "Furkan", "Cagri", "Kemal" };
+            // Error State exceeds student3 index limit
+            //students3[3] = "Mustafa";
+
+            foreach (var student in students3)
+            {
+                Console.WriteLine(student);
+                Console.WriteLine("*********");
+            }
+        // Multi-Dimensional Array
+            string[,] regions=new string[5,3]
+            {
+                {"İstanbul","İzmit","Balıkesir" },
+                {"Ankara","Konya","Kırıkkale" },
+                {"Antalya","Adana","Mersin" },
+                {"Rize","Trabzon","Samsun" },
+                {"İzmir","Muğla","Manisa" }
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("*********");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager=new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager=new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer=new Customer();
+            customer.City = "İstanbul";
+            customer.Id = 1;
+            customer.FirstName = "Furkan";
+            customer.LastName = "Alkan";
+
+            Customer customer2 = new Customer
+            {
+                Id = 2, City = "İstanbul", FirstName = "Kemal", LastName = "Usta"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+        }
+    }
+}
