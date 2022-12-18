0d715ca96afefbbe5a79e2c01a5637fe7273c41e
falkanx <116848375+falkanx@users.noreply.github.com>
2022-12-18 19:49:05 +0300

    Add files via upload

---
+++ RecapDemo2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creator creator=new Creator();
+            creator.Maker =new Animal();
+            creator.Add();
+            Console.ReadLine();
+        }
+    }
+
+    class Creator
+    {
+        public IMaker Maker { get; set; }
+        public void Add()
+        {
+            Maker.Log();
+            Console.WriteLine("Living beings added!");
+        }
+    }
+
+    class Animal:IMaker
+    {
+        public void Log()
+        {
+            Console.WriteLine("Animal Created!");
+        }
+    }
+
+    class Monster : IMaker
+    {
+        public void Log()
+        {
+            Console.WriteLine("Monster Created!");
+        }
+    }
+
+    class Human : IMaker
+    {
+        public void Log()
+        {
+            Console.WriteLine("Human Created!");
+        }
+    }
+
+    interface IMaker
+    {
+        void Log();
+    }
+}
---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Value Types
+            // Console.WriteLine("Hello World!");
+            // double used to hold decimal numbers
+            double number5 = 10.4;
+            // decimal used to convert integers to decimals
+            decimal number6 = 10;
+            // char
+            char character = 'A';
+            // conditions True Or False
+            bool condition = false;
+            // byte max value 0 and 255
+            byte number4 = 255;
+            // short max value 32767 and -32768
+            short number3 = 32767;
+            // int max value 2147483647 and -2147483648
+            int number1 = 2147483647;
+            // long max value 9223372036854775807 and -9223372036854775808
+            long number2 = 9223372036854775807;
+            // var keeps the value assigned to the variable in the value's type.
+            var number7 = 10;
+            // number7 = "A";
+            
+            Console.WriteLine("Number1 is {0}",number1);
+            Console.WriteLine("Number2 is {0}", number2);
+            Console.WriteLine("Number3 is {0}", number3);
+            Console.WriteLine("Number4 is {0}", number4);
+            Console.WriteLine("Number5 is {0}", number5);
+            Console.WriteLine("Number6 is {0}", number6);
+            Console.WriteLine("Number7 is {0}", number7);
+            Console.WriteLine("condition {0}", condition);
+            Console.WriteLine("Character is : {0}",(int)character);
+            Console.WriteLine((int)Days.Friday);
+            Console.ReadLine();
+        }
+    }
+
+    enum Days
+    {
+        Monday=10,Tuesday=20,Wednesday=30,Thursday,Friday,Saturday,Sunday
+    }
+}
