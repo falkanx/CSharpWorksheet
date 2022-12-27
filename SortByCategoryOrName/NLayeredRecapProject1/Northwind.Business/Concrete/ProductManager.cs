5e388dbacc98b0be01a42ffc3d1286346306a271
Furkan Alkan <116848375+falkanx@users.noreply.github.com>
2022-12-27 05:54:20 +0300

    Add files via upload

---
+++ SortByCategoryOrName/NLayeredRecapProject1/Northwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Business.Abstract;
+using Northwind.Business.ValidationRules.FluentValidation;
+using Northwind.DataAccess.Abstract;
+using FluentValidation;
+using Northwind.Business.Utilities;
+using Northwind.DataAccess.Concrete.EntityFramework;
+using Northwind.Entities.Concrete;
+using ValidationException = FluentValidation.ValidationException;
+
+namespace Northwind.Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+            ValidationTool.Validate(new ProductValidator(), product);
+            _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            try
+            {
+                _productDal.Delete(product);
+            }
+            catch
+            {
+                throw new Exception("The product couldn't be Deleted!");
+            }
+
+        }
+
+        public List<Product> GetAll()
+        {
+            //Business Code
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetProductsByCategory(int categoryId)
+        {
+            return _productDal.GetAll(p => p.CategoryId == categoryId);
+        }
+
+        public object GetProductsByProductName(string productName)
+        {
+            return _productDal.GetAll(p => p.ProductName.ToLower().Contains(productName.ToLower()));
+        }
+
+        public void Update(Product product)
+        {
+            ValidationTool.Validate(new ProductValidator(), product);
+            _productDal.Update(product);
+        }
+    }
+}
---
+++ SortByCategoryOrName/NLayeredRecapProject1/Northwind.Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Entities.Concrete;
+using FluentValidation;
+
+namespace Northwind.Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty().WithMessage("Must not be empty!");
+            RuleFor(p => p.CategoryId).NotEmpty().WithMessage("Must not be empty!");
+            RuleFor(p => p.UnitPrice).NotEmpty().WithMessage("Must not be empty!");
+            RuleFor(p => p.QuantityPerUnit).NotEmpty().WithMessage("Must not be empty!");
+            RuleFor(p => p.UnitsInStock).NotEmpty().WithMessage("Must not be empty!");
+
+            RuleFor(p => p.UnitPrice).GreaterThan(0).WithMessage("Unit price must be greater than 0!");
+            RuleFor(p => p.UnitsInStock).GreaterThanOrEqualTo((short)0);
+            RuleFor(p => p.UnitPrice).GreaterThan(10).When(p => p.CategoryId == 2).WithMessage("Condiments unit price must be greater than 10!");
+
+            RuleFor(p => p.ProductName).Must(StartWithUpper).
+                WithMessage("The first letter of the product you add must be capitalized!");
+        }
+
+        private bool StartWithUpper(string arg)
+        {
+            char character = arg[0];
+            if (char.IsUpper(character))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
